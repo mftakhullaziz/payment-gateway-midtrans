1594cd783bc8fbac87dfeb900c1bcc9390d8d815
mftakhullaziz <azizmiftakhul19@gmail.com>
2025-03-30 02:59:29 +0700

    fix secret

---
+++ env.sh
@@ -0,0 +1,3 @@
+export DB_URL="jdbc:mysql://mysql-dev-cluster1-mftcoder-433c.e.aivencloud.com:18253/example_payment_midtrans?ssl-mode=REQUIRED"
+export DB_USERNAME="avnadmin"
+export DB_PASSWORD="AVNS_4gVt0bTLKPSHfF_Ri2P"
