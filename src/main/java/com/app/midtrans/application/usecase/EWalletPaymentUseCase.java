96263d434064045a1d62b19be93c78239294b5bd
aizacoders <azizmiftakhul19@gmail.com>
2026-01-19 21:38:44 +0700

    feat: update code

--- src/main/java/com/app/midtrans/application/usecase/EWalletPaymentUseCase.java
+++ src/main/java/com/app/midtrans/application/usecase/EWalletPaymentUseCase.java
@@ -1,10 +1,14 @@
 package com.app.midtrans.application.usecase;
 
 import com.app.midtrans.application.UseCaseExecutor;
+import com.app.midtrans.domain.customer.Customer;
+import com.app.midtrans.domain.customer.CustomerPersistencePort;
+import com.app.midtrans.domain.customer.CustomerService;
 import com.app.midtrans.domain.payment.PaymentGatewayPort;
 import com.app.midtrans.domain.payment.PaymentService;
 import com.app.midtrans.infra.adapter.inbound.request.EWalletTransferRequest;
 import com.app.midtrans.infra.adapter.inbound.response.EWalletTransferResponse;
+import com.app.midtrans.shared.enums.EWalletChannel;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Component;
 
@@ -16,8 +20,16 @@ public class EWalletPaymentUseCase
     private final PaymentGatewayPort paymentGatewayPort;
     private final PaymentService paymentService;
 
+    private final CustomerPersistencePort customerPersistencePort;
+    private final CustomerService customerService;
+
     @Override
     public void execute(EWalletTransferRequest input, EWalletTransferResponse output) {
+        Customer customer = customerPersistencePort.getCustomerById(input.getCustomerId());
+        customerService.ensureEligibleForPayment(customer);
+
+        EWalletChannel channel = EWalletChannel.fromPaymentType(input.getPaymentType());
+
 
     }
 
